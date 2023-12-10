29804d662f49e3c3b7726a07ae6dd83ee443d669
carterawebb <carterwebb@nevada.unr.edu>
2023-12-10 14:23:57 -0800

    FInishing the AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,58 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.Audio;
 using System;
-//using UnityEditor.SearchService;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour
-{
+public class AudioManager : MonoBehaviour {
+    public Sound[] sounds;
+
     public static AudioManager instance;
 
-    public Sound[] musicSounds;
-    public AudioSource musicSource;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
+    public void Awake() {
+        if (instance == null) {
             instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
+        } else {
             Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
-    public void Start()
-    {
-        string activeScene = SceneManager.GetActiveScene().name;
-
-        if (activeScene == "MainMenu" || activeScene == "LevelSelect")
-        {
-            PlayMusic("MenuMusic");
-        }
-        else
-        {
-            PlayMusic("GameMusic");
+    private void Start() {
+        if (SceneManager.GetActiveScene().name == "MainMenu" || SceneManager.GetActiveScene().name == "LevelSelect") {
+            Play("MenuMusic");
+        } else {
+            Play("GameMusic");
         }
     }
 
-    public void PlayMusic(string name)
-    {
-        Sound s = Array.Find(musicSounds, sound => sound.soundName == name);
-        if (s == null)
-        {
+    private void Update() {
+        if ((SceneManager.GetActiveScene().name == "MainMenu" || SceneManager.GetActiveScene().name == "LevelSelect") && GetCurrentlyPlaying() != "MenuMusic") {
+            Stop("Gameplay");
+
+            Play("MainMenu");
+        } else if (SceneManager.GetActiveScene().name != "MainMenu" && SceneManager.GetActiveScene().name != "LevelSelect" && GetCurrentlyPlaying() != "GameMusic") {
+            Stop("MenuMusic");
+
+            Play("GameMusic");
+        }
+    }
+
+    public void Play(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) {
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
-        else
-        {
-            musicSource.clip = s.clip;
-            musicSource.Play();
+
+        /*
+        if (UIManager.GameIsPaused) {
+            s.source.Pause();
+            return;
+        } else {
+            s.source.UnPause();
         }
+        */
+
+        s.source.Play();
     }
 
+    public void Stop(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
 
+        s.source.Stop();
+    }
+
+    public void SetVolume(string name, float volume) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        s.source.volume = volume;
+    }
+
+    public float GetVolume(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        return s.source.volume;
+    }
+
+    public string GetCurrentlyPlaying() {
+        foreach (Sound s in sounds) {
+            if (s.source.isPlaying) {
+                return s.name;
+            }
+        }
+
+        return null;
+    }
 }
